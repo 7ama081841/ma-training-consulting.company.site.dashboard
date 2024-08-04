1c4247f3aca61701ac4a26ffd247695f4ad7462b
mohamed123 <chaabenim318@gmail.com>
2024-08-04 13:39:03 +0100

    update vercel.json

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
-import { BASE_URL } from './src/config/constant';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -60,8 +59,8 @@ export default defineConfig(({ mode }) => {
         ]
       }
     },
-    // base: API_URL,
-    base: BASE_URL,
+    base: API_URL,
+
     plugins: [react(), jsconfigPaths()]
   };
 });
