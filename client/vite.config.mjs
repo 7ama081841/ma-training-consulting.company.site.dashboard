ca3c906146c8bb99ec0a379f7407a9a80df6af44
mohamed123 <chaabenim318@gmail.com>
2024-08-04 13:27:11 +0100

    update vite.config

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
+import { BASE_URL } from './src/config/constant';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -59,7 +60,8 @@ export default defineConfig(({ mode }) => {
         ]
       }
     },
-    base: API_URL,
+    // base: API_URL,
+    base: BASE_URL,
     plugins: [react(), jsconfigPaths()]
   };
 });
